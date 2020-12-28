0a5790c756117f5161ec54253ee25a3a9947d8ba
Sagar <smendira@nextnav.com>
2020-12-28 23:43:14 +0530

    Added Flask application

---
+++ EB-Flask/application.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+def get_response(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+header_text = ''' <html>EB Flask Test<body>'''
+instructions = ''' '''
+footer_text = '</body>\n</html>'
+
+application = Flask(__name__)
+
+application.add_url_rule('/', 'index', (lambda: header_text + get_response() + instructions + footer_text))
+application.add_url_rule('/<username>', 'hello', (lambda username:  header_text + get_response(username) + footer_text))
+
+if __name__ == "__main__":
+   application.debug = True
+   application.run()
