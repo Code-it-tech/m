f75277f9ebf600ada7bb3c7f6380d0e64af82b33
Sagar <smendira@nextnav.com>
2020-12-29 01:20:02 +0530

    Added

--- AWS S3.py
+++ AWS S3.py
@@ -11,3 +11,12 @@ bucket_list = s3client.list_buckets()
 for bucket in bucket_list["Buckets"]:
    if bucket['Name'] == bucket_name:
        print("I just Created a new bucket named:{} at {}".format(bucket['Name'],bucket['CreationDate']))
+
+object_key = "s3.pdf"
+s3client.put_object(Bucket=bucket_name,Key=object_key,Body="Uploading the PDF")
+print("Uploaded the PDF: {} succesfully to {}".format(object_key,bucket_name))
+
+# Generate the PRE Signed URL
+
+url = s3client.generate_presigned_url('get_object', {'Bucket': bucket_name,'Key': object_key})
+print("Try this URL {}".format(url))
