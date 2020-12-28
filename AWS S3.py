1606586a4e8b6b4c91d54de94e0e31a56a80694b
Sagar <smendira@nextnav.com>
2020-12-28 16:19:09 +0530

    Added AWS-s3

---
+++ AWS S3.py
@@ -0,0 +1,8 @@
+ # boto3 offers 2 types of API's  a) Resource API (high level) works on object oriented object.delete and the other is b) Client API(Low Level) such as it maps directly like put_objects
+
+import boto3
+
+s3client = boto3.client('s3')
+bucket_name = "python-sdk-sample11"
+print('Creating new bucket {}'.format(bucket_name))
+s3client.create_bucket(Bucket=bucket_name)
